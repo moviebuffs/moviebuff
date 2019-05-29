eb9595119167a80ea1d1e045184e10c2e3decd20
EricaMcc <emccul13@gmail.com>
2019-05-28 22:37:13 -0500

    added basic src/Components/Search.js

---
+++ frontend/src/Components/Search.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../App.css';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
+}
+
+export default Search;
