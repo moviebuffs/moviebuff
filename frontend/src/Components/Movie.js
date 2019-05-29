cd0727a7b50d02d2f7bfc18b218f94ceec7b2e41
EricaMcc <emccul13@gmail.com>
2019-05-29 11:56:11 -0500

    created movie component

---
+++ frontend/src/Components/Movie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  render() {
+    return (
+      <div>
+        {/* information about movie from api data */}
+      </div>
+    );
+  }
+}
+
+export default Movie;
