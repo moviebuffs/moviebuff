3db2329e7f237762b554cd1b8ab0dc562b514ced
EricaMcc <emccul13@gmail.com>
2019-05-29 12:37:21 -0500

    added component for displaying list of tweets

---
+++ frontend/src/Components/TweetList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../App.css';
+import Tweet from './Tweet'
+
+class TweetList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+  }
+
+  render() {
+    return (
+      <div>
+        {/* map tweets here */}
+      </div>
+    );
+  }
+}
+
+export default TweetList;
