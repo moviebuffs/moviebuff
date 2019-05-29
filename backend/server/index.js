136b3d760911b392da41b63175b732b828a1cda5
samualmoot <samual.r.moot@gmail.com>
2019-05-29 12:48:57 -0500

    sequelized our database

--- backend/server/index.js
+++ backend/server/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require('axios');
+
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
@@ -15,13 +15,9 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
 });
-axios.get('https://api.themoviedb.org/3/search/movie', {
-    params: {
-        api_key: 'f2e2040f39de7b736d7468ad02b4f3c7',
-        query: 'Saw',
-    }
-}).then((response) => {
-    console.log(response);
+
+app.get('/movie', (req, res) => {
+    
 })
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
