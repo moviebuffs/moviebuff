f36016da90c6bb2fc2e2ff7de0e895f715d0b79b
EricaMcc <emccul13@gmail.com>
2019-05-30 19:25:39 -0500

    created handler for now playing movie requests

--- backend/server/helpers/index.js
+++ backend/server/helpers/index.js
@@ -9,4 +9,25 @@ axios.get('https://api.themoviedb.org/3/search/movie', {
     console.log(response);
 }).catch((err) => {
     console.error(err);
-})           
+})
+
+const nowPlaying = () => {
+    return axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+        params: {
+            api_key: 'f2e2040f39de7b736d7468ad02b4f3c7',
+            language: 'en-US',
+            page: 1,
+            region: 'US',
+        }
+    })
+    .then((response) => {
+        console.log(response);
+        return response;
+    }).catch((err) => {
+        console.error(err);
+    })
+};
+
+// nowPlaying();
+
+module.exports.nowPlaying = nowPlaying;
--- backend/server/index.js
+++ backend/server/index.js
@@ -9,9 +9,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const { nowPlaying } = require('./helpers/index');
+
 app.use(express.static(path.join(__dirname, "../../frontend/public")));
 app.use(bodyParser.json());
 
+app.get('/now-playing', (req, res) => {
+    nowPlaying().then((playingMovies) => {
+        console.log('HIT', playingMovies);
+        const { results } = playingMovies.data;
+        const currentMovies = results.map((movie) => {
+            return {originalTitle: movie.original_title,
+            overview: movie.overview,
+            posterPath: movie.poster_path,
+            voteAvg: movie.vote_average,
+            // voteCount: movie.cote_count,
+            }
+        });
+        console.log('CURRENTMOVIES', currentMovies);
+        res.json(currentMovies);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.sendStatus(500);
+    })
+});
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 app.listen(port, hostname, () => {
