befd73e8bbf3e4ffb50d08d555a3754c6705115f
EricaMcc <emccul13@gmail.com>
2019-06-01 23:48:48 -0500

    commit to pull

--- database/index.js
+++ database/index.js
@@ -10,9 +10,9 @@ sequelize.authenticate()
   .then(() => console.log('Connected to the database'))
   .catch(err => console.error('Could not connect to the database', err));
 
-sequelize.sync({
-  force: true, // Drops info in database for testing 
-})
+// sequelize.sync({
+//   force: true, // Drops info in database for testing 
+// })
 
 const User = sequelize.define('user', { // model schema for user -- lowercase for psql. 
   username: Sequelize.STRING,
