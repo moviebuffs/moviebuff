903a7a86d6f3baa32d33cb5dfa491fcd9321ce4b
Khari <khari.lane@khalikenterprises.com>
2019-06-03 13:53:18 -0500

    Finished Appbar with working buttons

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,37 +1,84 @@
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Button, Drawer } from '@material-ui/core/'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import SortIcon from '@material-ui/icons/Sort'
+import FormatIndentIcon from '@material-ui/icons/FormatIndentDecrease'
+
 import React, { Component } from 'react'
 
 export default class Navbar extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-      
+      open: false,
     }
+
+    this.handleDrawerClose = this.handleDrawerClose.bind(this);
+    this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
+    
   }
+
+  handleDrawerOpen() {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose() {
+    this.setState({ open: false });
+  };
+  
   
   render() {
+    const { open } = this.state;
     return (
-      <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <IconButton edge="start" color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h5" color="inherit">
-              Welcome, {this.props.user.displayName}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <button onClick={this.props.logoutClick}>Log Out</button>
-        <button onClick={this.props.goHome}>Home</button>
-        <button onClick={this.props.viewWatchlist}>View Watchlist</button>
-      </div>
+      <MuiThemeProvider >
+        <div>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+
+              <div>
+                <IconButton
+                  aria-label="Menu"
+                  onClick={this.handleDrawerOpen}
+                >
+                  <SortIcon />
+                </IconButton>
+              </div>
+              
+              <div>
+                <Typography variant="h5" color="inherit">
+                  Welcome, {this.props.user.displayName}
+                </Typography>
+              </div>
+
+              <Drawer
+                variant="persistent"
+                anchor="left"
+                open={open}
+              >
+                <div>
+                  <IconButton onClick={this.handleDrawerClose} >
+                    <FormatIndentIcon />
+                  </IconButton>
+                </div>
+
+                
+                <Button onClick={this.props.logoutClick}>Log Out</Button>
+                
+                <Button onClick={this.props.goHome}>Home</Button>
+                
+                <Button onClick={this.props.viewWatchlist}>View Watchlist</Button>
+      
+              </Drawer>
+            </Toolbar>
+          </AppBar>
+          
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
+
 
